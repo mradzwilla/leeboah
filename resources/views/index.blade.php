b7fbf7e602bc4c29aabd390165d5864448cc54ba
mradzwilla <mradzwilla@gmail.com>
2018-09-03 01:33:32 -0400

    Initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layout')
+
+@section('content')
+<div class="intro-container">
+	<div class="text-wrapper">
+		<h1>Lee Boah</h1>
+		<h2>Enter the Jungle</h2>
+	</div>
+	<div class="colored"></div>
+	<div class="gray"></div>
+</div>
+@endsection
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+	<title>Lee Boah</title>
+
+	<!-- Favicons Start -->
+
+	<!-- In case image.ico -->
+	<link rel="shortcut icon" type="image/x-icon" href="favicon.ico"  />
+
+	<!-- In case image.png -->
+	<link rel="shortcut icon" type="image/png" href="favicon.png" />
+
+	<!-- Favicons End -->
+
+	<!-- CSS Start -->
+	<link rel="stylesheet" type="text/css" href="css/normalize.css">
+	<link rel="stylesheet" type="text/css" href="css/global.css">
+	<!-- CSS End -->
+
+</head>
+<body>
+
+@yield('content')
+
+</body>
+</html>

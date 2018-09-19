5602c35aa49a6faf699ac8f1ee03302372b047d0
mradzwilla <mradzwilla@gmail.com>
2018-09-19 00:10:56 -0400

    Created deploy

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,8 +3,21 @@
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-125991282-1"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+
+	  gtag('config', 'UA-125991282-1');
+	</script>
 
 	<title>Lee Boah</title>
+
+	<meta name=viewport content="width=device-width, initial-scale=1">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  	<meta name="description" content="The official page of Lee Boah wrestling">
 
 	<!-- Favicons Start -->
 
